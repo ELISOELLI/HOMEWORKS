4cb4ddf17e5979960b9f7afc74d6ba3376ddac3f
ELISOELLI <110204836+ELISOELLI@users.noreply.github.com>
2022-08-29 22:58:42 +0300

    deleted extra lines
    deleted extra task

--- homeworksC#/Program.cs
+++ homeworksC#/Program.cs
@@ -473,30 +473,6 @@ else
     Console.WriteLine("точка пересечения "+"("+xIntersection+";" +yIntersection+ ")");
 }
 
-//TASK 47 SEMINAR 6 C#
-// Напишите программу, которая найдёт точку пересечения двух прямых, 
-// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-Console.WriteLine ("Введите значения b1, k1, b2, k2");
-Console.Write("Введите b1: ");
-double b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k1: ");
-double k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-double b2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-double k2 = Convert.ToDouble(Console.ReadLine());
-double xIntersection = 0;
-double yIntersection = 0;
-if (b1 == b2 & k1 == k2) Console.WriteLine("Прямые совпадают");
-else if (k1 == k2) Console.WriteLine("Прямые параллельны друг другу");
-else 
-{
-    xIntersection = (b2 - b1)/(k1 - k2);
-    yIntersection = k1*xIntersection + b1;
-    Console.WriteLine("точка пересечения "+"("+xIntersection+";" +yIntersection+ ")");
-}
 
 // TASK3 SEMINAR 6 C# (additional-доп.задача)
 // Задайте двумерный массив со случайными числами от -10 до 10.
