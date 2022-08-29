d56cf4d1cf216b891ab5cb4c87cd9b56a2e3e400
Eliso <9251481241@mail.ru>
2022-08-30 00:10:39 +0300

    ADDED THE TASKS47,50,52 OF THE SEMINAR 7 C# ON LINE 542

--- homeworksC#/Program.cs
+++ homeworksC#/Program.cs
@@ -537,3 +537,122 @@ Console.WriteLine($"Сумма чисел на главной диагонали
 Console.WriteLine("Случайный массив: ");
 Console.WriteLine(prettyPrint2dArray(randomArray));
 Console.WriteLine($"Сумма чисел на главной диагонали: {sumOfDiagonal(randomArray)}");
+
+
+//TASK47 SEMINAR 7 C#
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк: ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+//TASK50 SEMINAR 7 C#
+//  Напишите программу, которая на вход принимает число, и возвращает индексы этого элемента или же указание, 
+// что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+  
+int[,] array = new int[3, 4];
+ 
+    Random random = new Random();
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(10);
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+        }
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool f = false;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (number == array[i, j]) { f = true; };
+        
+                }
+            }
+            if (f)
+                for(int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if(array[i,j] == number)
+                    {
+                        Console.WriteLine("Массив содержит число, его индекс: ({0},{1})", i, j);
+                    }
+                }
+            }
+            else
+                Console.WriteLine("Такого числа в массиве нет");
+
+
+//TASK 52 SEMINAR 7 C#
+// Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3
+
+int[,] Array = new int[3,4];
+ 
+            Random random = new Random();
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Array[i, j] = random.Next(10);
+                    Console.Write(Array[i, j] + "\t");
+                }
+                Console.WriteLine();
+}
+Console.WriteLine("Найдем среднее арифметическое элементов в каждом столбце:");
+for (int j = 0; j < Array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        sum += Array[i, j];
+    }
+    Console.Write($"{ sum / Array.GetLength(0)} ");
+}
+Console.ReadLine();
